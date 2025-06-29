0f7686e67a204c0e92cc731098a77e0c5c918761
Serhii Kuznets <testopesto7@gmail.com>
2025-06-29 19:59:02 +0200

    refator test step

--- tests/standardUserCompleteOrder.spec.ts
+++ tests/standardUserCompleteOrder.spec.ts
@@ -18,81 +18,39 @@ test(
   }) => {
     const productName = 'Sauce Labs Backpack';
 
-    await test.step('navigate to Login page', async () => {
+    await test.step('Login via standard user', async () => {
       await loginPage.navigateToLogin();
-    });
-
-    await test.step('fill Input Email using test use email also filling password data ', async () => {
       await loginPage.fillLoginInput(userLogin);
-    });
-
-    await test.step('click Sign In Button', async () => {
       await loginPage.pressSingUpBtn();
-    });
-
-    await test.step('Url should be changed after success Sign In', async () => {
       expect(page.url()).toContain('/inventory.html');
     });
 
-    await test.step('Checked visibility of title product page ', async () => {
+    await test.step('User add product to the cart and match price from item with cart price', async () => {
+      const item = inventoryPage.getProductItem(productName);
       await expect(inventoryPage.logoHeader).toBeVisible();
-    });
-
-    await test.step('Sort Product items from Low to Hi', async () => {
-      await inventoryPage.getSort('lohi');
-    });
-
-    const item = await test.step('get product from items on the Inventory Page', async () => {
-      return inventoryPage.getProductItem(productName);
-    });
-    const priceItem = await test.step('get product price for item from list', async () => {
-      return item.getItemPrice();
-    });
-    await test.step('add product to cart', async () => {
+      const priceItem = await item.getItemPrice();
       await item.addToCartByName();
-    });
-    await test.step('click to cart button', async () => {
       await inventoryPage.clickToCart();
-    });
-    await test.step('user should be on cart page ', async () => {
       expect(page.url()).toContain('/cart.html');
-    });
-    const cartItem = await test.step('get product items on the Cart Page  ', async () => {
-      return cartPage.getProductCartItem(productName);
-    });
-    const cartItemPrice = await test.step('get product price from the Cart Page  ', async () => {
-      return await cartItem.getItemPrice();
-    });
-    await test.step('Price from Inventory Page should be match with price from Cart', async () => {
+      const cartItem = cartPage.getProductCartItem(productName);
+      const cartItemPrice = await cartItem.getItemPrice();
       expect(priceItem).toContain(cartItemPrice);
-    });
-    await test.step('Navigate to the Checkout page by clicking the Checkout button', async () => {
       await cartPage.clickCheckOutBtn();
     });
-    await test.step('user should be on the Checkout page One', async () => {
-      expect(page.url()).toContain('/checkout-step-one.html');
-    });
 
-    await test.step('User filling checkout_info form', async () => {
+    await test.step('User Filled Address Form and going to second step', async () => {
+      expect(page.url()).toContain('/checkout-step-one.html');
       await checkoutStepOnePage.fillForm({
         'First Name': 'SK',
         'Last Name': 'TeST_NAME',
         'Zip/Postal Code': '43-502',
       });
-    });
-
-    await test.step('Click the Continue button', async () => {
       await checkoutStepOnePage.clickContinueBtn();
     });
-    await test.step('user should be on the Checkout page Two', async () => {
+
+    await test.step('Success Ordering Message displaying', async () => {
       expect(page.url()).toContain('/checkout-step-two.html');
-    });
-
-    await test.step('click finish button', async () => {
       await checkoutStepTwoPage.clickFinishBtn();
-    });
-
-    await test.step('CLick the Finish button , Compete Message should be present', async () => {
       const completeMessage = checkoutCompletePage.completeMsg;
       expect(completeMessage).toBeVisible();
     });
